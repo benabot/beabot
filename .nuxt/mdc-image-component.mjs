d895e561eba29c7b46669d89be28c70da720b5b6
benabot <benoitabot@yahoo.fr>
2025-12-16 10:35:19 +0100

    nuxt 3 - v1.0.1

---
+++ .nuxt/mdc-image-component.mjs
@@ -0,0 +1,1 @@
+export { default } from "/Users/benoitabot/Sites/beabot/node_modules/@nuxt/image/dist/runtime/components/NuxtImg.vue"
---
+++ .nuxt/prerender/chunks/build/useTags-B3XfhrJ_.mjs
@@ -0,0 +1,2 @@
+import{f as e}from"./server.mjs";const r=()=>{const t=e("currentTag",()=>"");return{tag:t,setTag:e=>{t.value=e},getTag:()=>t.value}};export{r};
+//# sourceMappingURL=useTags-B3XfhrJ_.mjs.map
